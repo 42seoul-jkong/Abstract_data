bd4f842400d931569c92c940d31bbcde89b25876
jkong <jkong@student.42seoul.kr>
2023-04-09 23:10:40 +0900

    variadic alias template

--- type_traits/make_void.hpp
+++ type_traits/make_void.hpp
@@ -5,7 +5,8 @@
 
 namespace ft
 {
-    template <typename T>
+    template <typename T1 = void, typename T2 = void, typename T3 = void, typename T4 = void, typename T5 = void,
+              typename T6 = void, typename T7 = void, typename T8 = void, typename T9 = void>
     struct make_void
     {
         typedef void type;
